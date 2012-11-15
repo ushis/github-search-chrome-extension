b71fb351c2948fb964a3fe4c2cfb64f42808e90a
ushi <ushi@porkbox.net>
2012-11-15 22:06:31 +0100

    Added @username syntax
    and rewrote everything...

--- github-search.js
+++ github-search.js
@@ -1,78 +1,104 @@
-// Lets go!
 (function($) {
 
-  // Set some general ajax options.
+  //
+  // Setup.
+  //
+
+  // Use localStorage cache for all ajax requests.
   $.ajaxSetup({
     dataType: 'json',
     localCache: true,
     cacheTTL: 6 // hours
   });
 
-  // Our big fat GitHub object.
-  var GitHub = {
+  // XML sanitization rules.
+  var sanitizationRules = [
+    [/&/g, '&amp;'],
+    [/</g, '&lt;'],
+    [/>/g, '&gt;'],
+    [/"/, '&quot;']
+  ];
 
-    // Base uri of github.
-    baseUri: 'https://github.com/',
+  // Chrome displays max 5 results.
+  var maxResults = 5;
 
-    // Some api uris.
-    apiUris: {
-      user: 'https://api.github.com/users/:user/repos?sort=pushed',
-      search: 'https://api.github.com/legacy/repos/search/:query'
+  // GitHub base urls.
+  var baseLocations = {
+    html: 'https://github.com',
+    api: 'https://api.github.com'
+  };
+
+  // Some GitHub paths we need.
+  var locations = {
+    html: {
+      user: '/:user',
+      repo: '/:user/:repo',
+      search: '/search?q=:query'
     },
-
-    // Asks GitHub for some data.
-    //
-    //   GitHub.askFor('rails', function(data) { console.log(data); });
-    //
-    // To search the repos of a user, write a query like 'username/rails'.
-    askFor: function(query, callback) {
-      if (query.indexOf('/') > -1) {
-        this.askForUser(query, callback);
-      } else {
-        this.askForRepos(query, callback);
+    api: {
+      users: {
+        repos: '/users/:user/repos?sort=pushed',
+        search: '/legacy/user/search/:query'
+      },
+      repos: {
+        search: '/legacy/repos/search/:query'
       }
-    },
-
-    // Searches the repo of a user.
-    askForUser: function(query, callback) {
-      var parts = query.split('/');
-      var uri = this.apiUris.user.replace(':user', parts.shift());
-      query = parts.join('/');
-
-      $.getJSON(uri, function(data) {
-        var results = data.filter(function(repo) {
-          return repo.name.indexOf(query) > -1
-        }).map(function(repo) {
-          return {
-            url: repo.html_url,
-            name: repo.name,
-            user: repo.owner.login,
-            desc: (repo.description || '').slice(0, 66)
-          };
-        });
-
-        callback(results);
-      });
-    },
-
-    // Searches the repos.
-    askForRepos: function(query, callback) {
-      var self = this;
-      var uri = this.apiUris.search.replace(':query', encodeURIComponent(query));
-
-      $.getJSON(uri, function(data) {
-        var results = data.repositories.map(function(repo) {
-          return {
-            url: self.baseUri + repo.username + '/' + repo.name,
-            name: repo.name,
-            user: repo.username,
-            desc: (repo.description || '').slice(0, 66)
-          };
-        });
-
-        callback(results);
-      });
     }
+  };
+
+  //
+  // Utils.
+  //
+
+  // Returns the url for an Array of keys or a dotted path.
+  //
+  //   urlFor('api.users.search', { query: 'torvalds' });
+  //   //=> 'https://api.github.com/legacy/user/search/torvalds'
+  //
+  // The second argument is a object of with.
+  var urlFor = function(keys, params) {
+    var path = locations;
+
+    if ( ! (keys instanceof Array)) {
+      keys = keys.split('.');
+    }
+
+    keys.forEach(function(key) {
+      path = path[key];
+    });
+
+    for (var param in params) {
+      path = path.replace(':' + param, encodeURIComponent(params[param]));
+    }
+
+    return baseLocations[keys[0]] + path;
+  };
+
+  // Sanitizes everything in an object.
+  var sanitize = function(obj) {
+    if (obj === null || obj === undefined) {
+      return '';
+    }
+
+    if (typeof(obj) === 'number') {
+      obj = String(obj);
+    }
+
+    if (typeof(obj) === 'string') {
+      for (var rule in sanitizationRules) {
+        obj = obj.replace(rule[0], rule[1]);
+      }
+
+      return obj;
+    }
+
+    var _obj = {};
+
+    for (var attr in obj) {
+      _obj[attr] = sanitize(obj[attr]);
+    }
+
+    return _obj;
   };
 
   // Debounces a function.
@@ -85,66 +111,136 @@
 
       timeout = setTimeout(function() {
         callback.apply(self, args);
-        timeout = 0;
       }, delay);
 
       return this;
     };
   };
 
-  // Escapes some special XML chars.
-  var xmlEncode = function(string) {
-    return string.replace(/&/g, '&amp;')
-                 .replace(/</g, '&lt;')
-                 .replace(/>/g, '&gt;')
-                 .replace(/"/g, '&quot;');
-  };
+  //
+  // Result formatters.
+  //
 
-  // Wrappes found substrings in <match> tags.
-  var highlight = function(string, substring) {
-    return string.replace(substring, '<match>' + substring + '</match>');
-  };
-
-  // Creates a description for a repo.
-  var descriptionFor = function(repo, query) {
-    var _repo = {}, desc = '<url>';
-
-    for (var attr in repo)
-      _repo[attr] = xmlEncode(repo[attr]);
-
-    if (query.indexOf('/') === -1) {
-      query = xmlEncode(query);
-      desc += highlight(_repo.user + '/' + _repo.name, query);
-    } else {
-      query = xmlEncode(query.split('/').slice(1).join('/'));
-      desc += _repo.user + '/' + highlight(_repo.name, query);
+  // Wrappes substrings in <match> tags.
+  var highlight = function(string, query) {
+    if (query === '') {
+      return string;
     }
 
-    return desc + '</url> <dim>' + highlight(_repo.desc, query) + '</dim>';
+    var regex = query.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
+
+    return string.replace(RegExp(regex, 'gi'), '<match>$&</match>');
   };
 
-  // Catch the input.
-  chrome.omnibox.onInputChanged.addListener(debounce(function(query, callback) {
-    if ( ! query) return;
+  // Builds a user description.
+  var userDescription = function(user, query) {
+    user = sanitize(user), query = sanitize(query);
 
-    GitHub.askFor(query, function(data) {
-      var results = data.slice(0, 5).map(function(repo) {
+    return '<url>@' + highlight(user.login, query) + '</url> <dim>' +
+           highlight(user.name , query) + '</dim>';
+  };
+
+  // Builds a repo description.
+  var repoDescription = function(repo, query, highlightUser) {
+    repo = sanitize(repo), query = sanitize(query);
+    var user = highlightUser ? highlight(repo.user, query) : repo.user;
+
+    return '<url>' + user + '/' + highlight(repo.name, query) + '</url> <dim>' +
+           highlight(repo.desc.slice(0, 60), query) + '</dim>';
+  };
+
+  //
+  // Request methods.
+  //
+
+  // Processes a search request.
+  var request = function(query, callback) {
+    query = query.trim().toLowerCase();
+
+    if (query === '' || query === '@') {
+      return;
+    }
+
+    if (query[0] === '@') {
+      return findUsers(query.slice(1), callback);
+    }
+
+    if (query.indexOf('/') === -1) {
+      return findRepos(query, callback);
+    }
+
+    query = query.split('/');
+    findReposByUser(query.shift().trim(), query.join('/').trim(), callback);
+  };
+
+  // Finds users.
+  var findUsers = function(query, callback) {
+    $.getJSON(urlFor('api.users.search', { query: query }), function(data) {
+      var results = data.users.slice(0, maxResults).map(function(user) {
         return {
-          content: repo.url,
-          description: descriptionFor(repo, query)
+          content: urlFor('html.user', { user: user.username }),
+          description: userDescription({
+            login: user.username,
+            name: user.fullname
+          }, query)
         };
       });
 
       callback(results);
     });
-  }, 200));
+  };
 
-  // Catch the submission.
-  chrome.omnibox.onInputEntered.addListener(function (uri) {
-    if ( ! uri.match(/^http(?:s)?:\/\//)) {
-      uri = GitHub.baseUri + 'search?q=' + uri;
+  // Finds repos.
+  var findRepos = function(query, callback) {
+    $.getJSON(urlFor('api.repos.search', { query: query }), function(data) {
+      var results = data.repositories.slice(0, maxResults).map(function(repo) {
+        return {
+          content: urlFor('html.repo', { user: repo.username, repo: repo.name }),
+          description: repoDescription({
+            name: repo.name,
+            user: repo.username,
+            desc: repo.description
+          }, query, true)
+        };
+      });
+
+      callback(results);
+    });
+  };
+
+  // Finds repos by a user.
+  var findReposByUser = function(user, query, callback) {
+    $.getJSON(urlFor('api.users.repos', { user: user }), function(data) {
+      var results = data.filter(function(repo) {
+        return repo.name.toLowerCase().indexOf(query) > -1;
+      }).slice(0, maxResults).map(function(repo) {
+        return {
+          content: repo.html_url,
+          description: repoDescription({
+            name: repo.name,
+            user: repo.owner.login,
+            desc: repo.description
+          }, query, false)
+        };
+      });
+
+      callback(results);
+    });
+  };
+
+  //
+  // Chrome events.
+  //
+
+  // Process search request.
+  chrome.omnibox.onInputChanged.addListener(debounce(request, 250));
+
+  // Open the new page on enter.
+  chrome.omnibox.onInputEntered.addListener(function (url) {
+    if ( ! url.match(/^http(?:s)?:\/\//)) {
+      url = urlFor('html.search', { query: url });
     }
 
-    chrome.tabs.update({ url: uri });
+    chrome.tabs.update({ url: url });
   });
 })(jQuery);
